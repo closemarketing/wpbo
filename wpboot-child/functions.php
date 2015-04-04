805fa86162af51bf66500e982b5983ab94c3d093
David Perez <david@closemarketing.es>
2015-04-04 23:01:44 +0200

    Some WP Fixes

--- wpboot-child/functions.php
+++ wpboot-child/functions.php
@@ -4,9 +4,3 @@
  * @subpackage WPBoot Child Theme.
  * About: Add your awesome functions here.
 */
-
-// Load Parent Style Parent Theme - Fastest Way
-add_action( 'wp_enqueue_scripts', 'my_child_theme_scripts' );
-function my_child_theme_scripts() {
-wp_enqueue_style( 'parent-theme-css', get_template_directory_uri() . '/style.css' );
-}
