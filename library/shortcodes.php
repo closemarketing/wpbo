b83378f0206b6b095683d142e81c72194e8fab29
David Perez <david@closemarketing.es>
2015-06-09 23:13:58 +0200

    new shortcode links

--- library/shortcodes.php
+++ library/shortcodes.php
@@ -303,9 +303,31 @@ if ( !function_exists('btsc_carouselcpt_shortcode') ) {
 add_shortcode( 'carouselcpt', 'btsc_carouselcpt_shortcode' );
 }
 
-
-
-
+// Links shortcode
+if ( !function_exists('btsc_links_shortcode') ) {
+function btsc_links_shortcode($atts, $content = null) {
+    $args = array(
+        'orderby'          => 'name',
+        'order'            => 'ASC',
+        'limit'            => -1,
+        'hide_invisible'   => 1,
+        'show_updated'     => 0,
+        'echo'             => 1,
+        'categorize'       => 1,
+        'title_before'     => '<h2>',
+        'title_after'      => '</h2>',
+        'category_orderby' => 'name',
+        'category_order'   => 'ASC',
+        'class'            => 'linkcat',
+        'category_before'  => '<li id=%id class=%class>',
+        'category_after'   => '</li>' );
+    $output = "";
+    $output .= '<ul class="bookmarks_list mylinks">';
+    $output .= wp_list_bookmarks($args);
+    $output .= '</ul>';
+    }
+add_shortcode( 'links', 'btsc_links_shortcode' );
+}
 
 // Gallery shortcode
 
