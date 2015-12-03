7401ead8c0f35c84982bb5bedc6172d0bc510a0f
David Perez <david@closemarketing.es>
2015-12-03 11:55:24 +0100

    cleaning comments and responsive logo srcset

--- header.php
+++ header.php
@@ -1,11 +1,9 @@
 <!doctype html>
-
 <!--[if IEMobile 7 ]> <html <?php language_attributes(); ?>class="no-js iem7"> <![endif]-->
 <!--[if lt IE 7 ]> <html <?php language_attributes(); ?> class="no-js ie6"> <![endif]-->
 <!--[if IE 7 ]>    <html <?php language_attributes(); ?> class="no-js ie7"> <![endif]-->
 <!--[if IE 8 ]>    <html <?php language_attributes(); ?> class="no-js ie8"> <![endif]-->
 <!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
-
 <head>
     <meta charset="utf-8">
     <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
@@ -25,10 +23,7 @@
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
     <link rel="shortcut icon" href="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/img/favicon.png" />
 
-    <!-- wordpress head functions -->
     <?php wp_head(); ?>
-    <!-- end of wordpress head -->
-
 </head>
 
 <body <?php body_class(); ?>>
@@ -47,7 +42,7 @@
 
 
             <a class="navbar-brand" title="<?php echo get_bloginfo('description'); ?>" href="<?php echo home_url(); ?>">
-                <img src="<?php echo esc_url( get_stylesheet_directory_uri() );?>/images/logo.png" width="225" height="75" alt="<?php bloginfo('description');?>" />
+                <img src="<?php bloginfo('stylesheet_directory');?>/img/logo.png" srcset="<?php bloginfo('stylesheet_directory');?>/img/logom.png 480w,<?php bloginfo('stylesheet_directory');?>/img/logo.png 2000w" alt="<?php bloginfo('description');?>" />
             </a>
             </div>
 
