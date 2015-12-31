fc7a005532469ed32fb8ddf699d1c41f7f6ef393
David Perez <david@closemarketing.es>
2015-12-31 11:24:31 +0100

    New Page Navigation in bootstrap

--- functions.php
+++ functions.php
@@ -153,6 +153,47 @@ function wpbo_list_pings($comment, $args, $depth) {
 <?php
 
 }
+/************* PAGE NAVI *****************/
+/* From http://www.kriesi.at/archives/how-to-build-a-wordpress-post-pagination-without-plugin */
+function wpbo_pagenavi($pages = '', $range = 2)
+{
+     $showitems = ($range * 2)+1;
+
+     global $paged;
+     if(empty($paged)) $paged = 1;
+
+     if($pages == '')
+     {
+         global $wp_query;
+         $pages = $wp_query->max_num_pages;
+         if(!$pages)
+         {
+             $pages = 1;
+         }
+     }
+     if(1 != $pages)
+     {
+         echo '<nav><ul class="pagination">';
+         if($paged > 2 && $paged > $range+1 && $showitems < $pages)
+            echo "<li><a href='".get_pagenum_link(1)."'>&laquo;</a></li>";
+         if($paged > 1 && $showitems < $pages)
+            echo "<li><a href='".get_pagenum_link($paged - 1)."' aria-label='Previous'><span aria-hidden='true'>&lsaquo;</span></a></li>";
+
+         for ($i=1; $i <= $pages; $i++)
+         {
+             if (1 != $pages &&( !($i >= $paged+$range+1 || $i <= $paged-$range-1) || $pages <= $showitems ))
+             {
+                 echo ($paged == $i)? "<li class='active'><span class='current'>".$i."</span></li>":"<li><a href='".get_pagenum_link($i)."' class='inactive' >".$i."</a></li>";
+             }
+         }
+         if ($paged < $pages && $showitems < $pages)
+            echo "<li><a href='".get_pagenum_link($paged + 1)."'>&rsaquo;</a></li>";
+         if ($paged < $pages-1 &&  $paged+$range-1 < $pages && $showitems < $pages)
+            echo "<li><a href='".get_pagenum_link($pages)."' aria-label='Next'><span aria-hidden='true'>&raquo;</span></a></li>";
+
+         echo "</ul></nav>\n";
+     }
+}
 
 /************* SEARCH FORM LAYOUT *****************/
 
