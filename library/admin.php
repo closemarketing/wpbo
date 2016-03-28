40a53c852d0e23926c9315dfb68dc8b45a677e94
David Perez <david@closemarketing.es>
2016-03-28 09:23:40 +0200

    solved include

--- library/admin.php
+++ library/admin.php
@@ -54,8 +54,8 @@ http://digwp.com/2010/10/customize-wordpress-dashboard/
 // RSS Dashboard Widget
 function wpbo_bootstrap_rss_dashboard_widget() {
 	if(function_exists('fetch_feed')) {
-		include_once(ABSPATH . WPINC . '/feed.php');               // include the required file
-		$feed = fetch_feed('http://themble.com/feed/rss/');        // specify the source feed
+		if ( is_readable($locale_file) ) include_once(ABSPATH . WPINC . '/feed.php');  // include the required file
+		$feed = fetch_feed('https://http://www.closemarketing.es/feed/');        // specify the source feed
 		$limit = $feed->get_item_quantity(7);                      // specify number of items
 		$items = $feed->get_items(0, $limit);                      // create an array of items
 	}
